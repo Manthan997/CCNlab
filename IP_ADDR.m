6ddbf7124b63e92d4ece217af9eb74098f416191
WannabeProdigy123 <145684914+WannabeProdigy123@users.noreply.github.com>
2024-04-23 21:11:54 +0530

    Update IP_ADDR.m
    Made the code and output much cleaner

--- IP_ADDR.m
+++ IP_ADDR.m
@@ -11,24 +11,30 @@ d = input('ENTER THE 4 byte: ');
 if 0 <= a && a <= 255 && 0 <= b && b <= 255 && 0 <= c && c <= 255 && 0 <= d && d <= 255
   %%valid ip%%
   if 0<= a && a <= 127
-    disp('Class A')
+  printf("\n%d.%d.%d.%d\n" , a , b , c , d )
+    disp('Given IP address is of Class A')
   endif
 
   if 128<= a && a <= 191
-    disp('Class B')
+  printf("\n%d.%d.%d.%d\n" , a , b , c , d )
+    disp('Given IP address is of Class B')
   endif
 
   if 192<= a && a <= 223
-    disp('Class C')
+  printf("\n%d.%d.%d.%d\n" , a , b , c , d )
+    disp('Given IP address is of Class C')
   endif
 
   if 224<= a && a <= 239
-    disp('Class D')
+  printf("\n%d.%d.%d.%d\n" , a , b , c , d )
+    disp('Given IP address is of Class D')
   endif
 
   if 240<= a && a <= 255
-    disp('Class E')
+  printf("\n%d.%d.%d.%d\n" , a , b , c , d )
+    disp('Given IP address is of Class E')
   endif
 else
-  disp('INVALID IP')
+printf("\n%d.%d.%d.%d\n" , a , b , c , d )
+  disp('Given INVALID IP address')
 end
